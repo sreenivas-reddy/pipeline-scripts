15d9cb77d410556b57097c0bff0e9b7f18933522
Sreenivas Reddy <jasmin_corps@outlook.com>
2023-02-26 09:37:08 +0530

    New Changes

---
+++ scripted/pipeline-properties.groovy
@@ -0,0 +1,24 @@
+properties([
+        parameters([
+                string(name: 'country', defaultValue: 'India', description: 'Country Name'),
+                booleanParam(name: 'married', defaultValue: true, description: 'Marital Status'),
+                choice(name: 'gender', choices: ['Male', 'Female'], description: 'Gender')
+        ])
+])
+
+node() {
+    stage('Initialization Stage') {
+        echo 'Initializing...'
+        print "Country: ${params.country}"
+        print "Are You Married: ${params.married}"
+        print "Gender: ${params.gender}"
+    }
+
+    stage('Build Stage') {
+        echo 'Building...'
+    }
+
+    stage('Deploy Stage') {
+        echo 'Deploying...'
+    }
+}
